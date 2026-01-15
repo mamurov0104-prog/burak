01cff5bf15a033eae5e70b7d7d39c7c133d4717c
RobertMamur <mamurov0104@gmail.com>
2026-01-15 12:56:22 +0500

    feat:connect mongoDB via Mongooose!

--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,127 @@
-console.log("working properly ! ...");
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then(data=>{
+console.log("MongoDb connection succeed");
+const PORT = process.env.PORT ?? 3003;
+})
+.catch(err=>console.log("ERROR on connection MongoDb", err));
+// console.log("PORT :", process.env.PORT);
 
-const person: string = "Robert";
-const count: number = 23;
-console.log(`My name is ${person} and I am ${count} old ....`)
-// import moment from "moment/ts3.1-typings/moment";
-import moment from "moment";
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime)
-// -------------------------------------
-class Person {
-    age:number;
-    firstName:string;
-    constructor(age:number,firstName:string){
-       this.age = age ;
-       this.firstName = firstName; 
-    }
-    getFullName():string{
-        return `Hi my name is ${this.firstName}`;
-    }
-}
-
-const person1 = new Person(20,"Hojiakbar") ;
-console.log(person1);
-
-// ------- Arrays : ------
-
-let numbers: number[] = [1, 2, 3];
-let users: string[] = ["Ali", "Vali"];
-
-// ------- object : type Anatation ----- interface korinishi emas bu 
- let user: {
-  name: string;
-  age: number;
-} = {
-  name: "Akbar",
-  age: 20
-};
+// Cluster => DataBase => Collection => Document
 
 
-// ---------- objecy (interface) : -----------------
 
-interface User {
-  name: string;
-  age: number;
-  email: string;
-}
 
-const u1: User = {
-  name: "Akbar",
-  age: 20,
-  email: "a@gmail.com"
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("working properly ! ...");
+
+// const person: string = "Robert";
+// const count: number = 23;
+// console.log(`My name is ${person} and I am ${count} old ....`)
+// // import moment from "moment/ts3.1-typings/moment";
+// import moment from "moment";
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime)
+// // -------------------------------------
+
+// // ------- Arrays : ------
+
+// let numbers: any[] = [1, 2, "3"];
+// let users: string[] = ["Ali", "Vali"];
+
+// // ------- object : type Anatation ----- interface korinishi emas bu 
+//  let user: { name: string; age: number;} = {
+//   name: "Akbar",
+//   age: 20
+// };
+
+
+// // ---------- objecy (interface) : -----------------
+
+// interface User {
+//   name: string;
+//   age: number;
+//   email: string;
+  
+// }
+
+// const u1: User = {
+//   name: "Akbar",
+//   age: 20,
+//   email: "a@gmail.com"
+// };
+
+
+// class Person{
+//     age:number;
+//     firstName:string;
+//     constructor(age:number,firstName:string){
+//        this.age = age ;
+//        this.firstName = firstName; 
+//     }
+//     getFullName():string{
+//         return `Hi my name is ${this.firstName}`;
+//     }
+// }
+
+// const person1 = new Person(20,"Hojiakbar") ;
+// console.log(person1);
+
+
