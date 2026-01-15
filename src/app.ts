7b49c821f1501a3db90116367d80fcbf42d9d5a6
RobertMamur <mamurov0104@gmail.com>
2026-01-15 13:35:27 +0500

    feat:burak project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+// -- ENTRANCE --
+const app = express();
+console.log("_dirname",__dirname);
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+// -- SESSIONS --
+// -- VIEWS --
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+// -- ROUTER --
+
+export default app; // = module.export = app;
--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,25 @@
-console.log("hebsbkbsjfsbfsbkjs")
+
+
+function getPositive1(arr: number[]): string {
+    // Natija saqlanadigan bo‘sh string yaratan,nkj
+    let result: string = "";
+
+    // Arrayning har bir elementini qolga olish kerak bu yerda 
+    for (let i = 0; i < arr.length; i++) {
+        let num: number = arr[i]; //qolga olingan element manshu hisobkkandi
+
+        // Agar element musbat bo‘lsa, uni stringga aylantirib natijaga qo‘shamiz
+        if (num > 0) {
+            result += num.toString();
+        }
+    }
+
+   
+    return result;
+}
+
+
+const numbers: number[] = [1, -4, 2];
+const positiveString: string = getPositive1(numbers);
+
+console.log("Musbat sonlar string ko‘rinishda:", positiveString); // "12"
