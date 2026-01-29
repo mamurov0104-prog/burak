f366fac7d496e95072d5ca7abfe512924d96c822
RobertMamur <mamurov0104@gmail.com>
2026-01-29 09:09:15 +0500

    Feat:integrate sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import {MORGAN_FORMAT} from "./libs/config"
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+import { Collection } from "mongoose";
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri:String(process.env.MONGO_URL),
+    collection: "session"
+})
 // -- ENTRANCE --
 const app = express();
 console.log("_dirname",__dirname);
@@ -12,6 +20,19 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 // -- SESSIONS --
+app.use(
+    session({
+        secret:String(process.env.SESSION_SECRET),
+        cookie:{
+            maxAge:1000 * 3600 * 3 , //3h
+        },
+        store:store, // agar false bolsa 10:30 access => 13:30 gacha / true da oxirgi kirgandan keyin uch soat mobaynida
+        resave:true,
+        saveUninitialized:true,
+    })
+
+
+)
 // -- VIEWS --
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
