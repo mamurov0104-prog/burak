53ccfaf898a6105062781d77cc651e977aea0a00
RobertMamur <mamurov0104@gmail.com>
2026-02-01 14:46:55 +0500

    all done

---
+++ src/schema/Product.model.ts
@@ -0,0 +1,54 @@
+import mongoose , {Schema} from "mongoose";
+import { ProductCollection, ProductStatus , ProductSize, ProductVolume} from "../libs/enums/product.enum";
+const productSchema = new Schema ({
+productStatus:{
+    type:String,
+    enum:ProductStatus,
+    default:ProductStatus.PAUSE,
+},
+productCollection:{
+    type:String,
+    enum:ProductCollection,
+    required:true,
+
+},productName:{
+    type:String,
+    required:true,
+    
+},productPrice:{
+    type:String,
+    required:true,
+    
+},productLeftCount:{
+    type:Number,
+    required:true,
+    
+},productSize:{
+    type:String,
+    enum:ProductSize,
+    default:ProductSize.NORMAL,  
+},productVolume:{
+    type:String,
+    enum:ProductVolume,
+    default:ProductVolume.ONE,  
+},productDesc:{
+    type:String,
+    required:true,
+    
+},productImage:{
+    type:[String],
+    default:[],    
+},productViews:{
+    type:Number,
+    default:0,
+}
+
+}
+,{timestamps:true}//updated at and created at
+);
+productSchema.index(
+    {productName: 1, ProductSize : 1 , ProductVolume: 1},
+    {unique:true}
+);
+
+export default mongoose.model('Product', productSchema);
--- src/train.ts
+++ src/train.ts
@@ -1,30 +1,12 @@
-// SquareNumber nomli type e'lon qilinyapti
-// Bu type bitta object qanday tuzilishga ega bo‘lishini belgilaydi
-// Ya'ni: har bir objectda number va square degan 2 ta property bo‘ladi
-type SquareNumber = {
-  number: number; // asl raqam
-  square: number; // o‘sha raqamning kvadrati
-};
-
-// getSquareNumbers functioni e'lon qilinyapti
-// arr parametri faqat number[] (raqamlar arrayi) bo‘lishi mumkin
-// function natijada SquareNumber[] (objectlar arrayi) qaytaradi
-function getSquareNumbers(arr: number[]): SquareNumber[] {
-
-  // map() metodi array ichidagi har bir elementni( korib chiqadh)
-  // bitta-bittadan olib, undan yangi element yasaydi
-  // va natijada yangi array qaytaradi
-  return arr.map(num => ({
-
-    // number property — array ichidagi hozirgi raqam
-    number: num,
-
-    // square property — shu raqamning kvadrati boya takidlaganimizdek 
-    square: num * num
-  }));
+function palindromCheck(word: string): boolean {
+  const reversed = word.split("").reverse().join("");
+  return word === reversed;
 }
 
-// call qismi 
-console.log(getSquareNumbers([1, 2, 3, 4, 5]));
+console.log(palindromCheck("dad")); 
+console.log(palindromCheck("kiyik")); 
+console.log(palindromCheck("kiyim")); 
+
+
 
 
