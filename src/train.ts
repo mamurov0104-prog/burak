784e11c6c2ba04039c0768a44b1f126da3a269c3
RobertMamur <mamurov0104@gmail.com>
2026-02-04 14:51:24 +0500

    fix:train.js to tessimnida !

--- src/train.ts
+++ src/train.ts
@@ -1,52 +1,72 @@
-function palindromCheck(word: string): boolean {
-  const reversed = word.split("").reverse().join("");
-  return word === reversed;
-}
 
-console.log(palindromCheck("dad")); 
-console.log(palindromCheck("kiyik")); 
-console.log(palindromCheck("kiyim")); 
+function objectToArray(obj: Record<string, any>): any[][] {
+  // Natijani saqlash uchun bo‘sh array hosil qilindi
+  const result: any[][] = [];
 
-
-
-// -------------------< yangi MIT task  >----------------
-
-// Har xil type (number, string, object, boolean va h.k.) bo‘lishi mumkin bo‘lgan
-// array qabul qiladigan function e’lon qilinyapti
-function calculateSumOfNumbers(arr: any[]): number {
-
-  // Sonlar yig‘indisini saqlash uchun boshlang‘ich qiymat
-  // Dastlab 0 ga teng
-  let sum = 0;
-
-  // for...of orqali array ichidagi har bir elementni bittadan olamiz
-  for (let item of arr) {
-
-    // Agar hozirgi elementning type'i "number" bo‘lsa
-    // faqat shundagina uni yig‘indiga qo‘shamiz
-    if (typeof item === "number") {
-
-      // sum ga item ni qo‘shamiz
-      sum += item;
-    }
-    // Agar item number bo‘lmasa (string, object, boolean va boshqalar)
-    // bu qator ishlamaydi va u element e’tiborga olinmaydi
+  // Object ichidagi har bir key bo‘yicha aylantiriladi
+  for (let key in obj) {
+    // Har bir key va uning qiymatini alohida array qilib qo‘shiladi
+    result.push([key, obj[key]]);
   }
 
-  // Barcha elementlar tekshirib bo‘lingach,
-  // faqat sonlardan iborat umumiy yig‘indini qaytaramiz
-  return sum;
+  // Hosil bo‘lgan arrayni kegin qaytaramia
+  return result;
 }
 
-// ----------- sinab korish  QISMI -----------
+// Test
+console.log(objectToArray({ a: 10, b: 20 }));
 
-// Functionga har xil type'lardan iborat array berilyapti:
-// 10        -> number (qo‘shiladi)
-// "10"      -> string (qo‘shilmaydi)
-// { son:10} -> object (qo‘shilmaydi)
-// true      -> boolean (qo‘shilmaydi)
-// 35        -> number (qo‘shiladi)
-console.log(
-  calculateSumOfNumbers([10, "10", { son: 10 }, true, 35])
-);
+
+
+// function palindromCheck(word: string): boolean {
+//   const reversed = word.split("").reverse().join("");
+//   return word === reversed;
+// }
+
+// console.log(palindromCheck("dad")); 
+// console.log(palindromCheck("kiyik")); 
+// console.log(palindromCheck("kiyim")); 
+
+
+
+// // -------------------< yangi MIT task  >----------------
+
+// // Har xil type (number, string, object, boolean va h.k.) bo‘lishi mumkin bo‘lgan
+// // array qabul qiladigan function e’lon qilinyapti
+// function calculateSumOfNumbers(arr: any[]): number {
+
+//   // Sonlar yig‘indisini saqlash uchun boshlang‘ich qiymat
+//   // Dastlab 0 ga teng
+//   let sum = 0;
+
+//   // for...of orqali array ichidagi har bir elementni bittadan olamiz
+//   for (let item of arr) {
+
+//     // Agar hozirgi elementning type'i "number" bo‘lsa
+//     // faqat shundagina uni yig‘indiga qo‘shamiz
+//     if (typeof item === "number") {
+
+//       // sum ga item ni qo‘shamiz
+//       sum += item;
+//     }
+//     // Agar item number bo‘lmasa (string, object, boolean va boshqalar)
+//     // bu qator ishlamaydi va u element e’tiborga olinmaydi
+//   }
+
+//   // Barcha elementlar tekshirib bo‘lingach,
+//   // faqat sonlardan iborat umumiy yig‘indini qaytaramiz
+//   return sum;
+// }
+
+// // ----------- sinab korish  QISMI -----------
+
+// // Functionga har xil type'lardan iborat array berilyapti:
+// // 10        -> number (qo‘shiladi)
+// // "10"      -> string (qo‘shilmaydi)
+// // { son:10} -> object (qo‘shilmaydi)
+// // true      -> boolean (qo‘shilmaydi)
+// // 35        -> number (qo‘shiladi)
+// console.log(
+//   calculateSumOfNumbers([10, "10", { son: 10 }, true, 35])
+// );
 
