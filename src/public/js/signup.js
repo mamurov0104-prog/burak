b4551b07f5f8367585d6ca9a36fa868158bf36e6
RobertMamur <mamurov0104@gmail.com>
2026-02-08 05:35:28 +0500

    Feat:signUp frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,42 @@
 console.log("Signup frontend javascript file");
+
+function validateSignupForm(){
+    // console.log("Executed validateSignupForm() ! ")
+const memberNick = $(".member-nick").val();
+const memberPhone = $(".member-phone").val();
+const memberPassword = $(".member-password").val();
+const confirmPassword = $(".confirm-password").val();
+if(
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === "" 
+){
+alert("please insert all requires inputs !")
+return false;
+}
+
+if(memberPassword !== confirmPassword){
+alert("Password differs , please check")
+return false;
+}
+
+
+
+// console.log("membernick :" , memberNick);    
+// return false;
+};
+
+
+
+
+
+
+
+
+$(function(){
+
+// $(".member-nick").click(function(){
+// alert($(".member-phone").toggle());
+// });
+});
