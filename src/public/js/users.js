e8ef2c899aa26eb3b660d6dc4a8fc295fc2789b5
RobertMamur <mamurov0104@gmail.com>
2026-02-10 07:31:21 +0500

    feat:user updateChosenUsers done

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,27 @@
-console.log("Users frontend javascript file");
+// const { default: axios } = require("axios");
+// const { response } = require("express");
+
+$(".member-status").on("change" , function(e){
+    const id = e.target.id.trim(); 
+    console.log("id" , id);
+
+    const memberStatus = $(`#${id}.member-status`).val();
+    console.log("memberStatus" , memberStatus);
+
+    axios.post("/admin/user/edit", {
+        _id:id,
+        memberStatus:memberStatus,
+    }).then((response)=>{
+        console.log("response :" , response);
+        const result = response.data;
+        if(result.data){
+            console.log("user updated");
+            $(".member-status").blur();
+        } else {
+            alert("user update failed !");
+        }
+    }).catch((err) =>{
+        console.log("Error : " , err)
+        alert("User update failed!")
+    })
+})
